330f6733fd5364072fef47ade39fecc5c7abf4d2
Neal Sanche <thorinside@gmail.com>
2026-01-04 18:11:54 -0700

    Add atan2 implementation for bare-metal ARM

--- src/stl_stubs.cpp
+++ src/stl_stubs.cpp
@@ -5,6 +5,10 @@
 
 #include <cmath>
 
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
 extern "C" double sinh(double x) {
     double ex = exp(x);
     return (ex - 1.0/ex) * 0.5;
@@ -13,6 +17,15 @@ extern "C" double sinh(double x) {
 extern "C" double tanh(double x) {
     double ex = exp(2.0 * x);
     return (ex - 1.0) / (ex + 1.0);
+}
+
+extern "C" double atan2(double y, double x) {
+    if (x > 0) return atan(y / x);
+    if (x < 0 && y >= 0) return atan(y / x) + M_PI;
+    if (x < 0 && y < 0) return atan(y / x) - M_PI;
+    if (x == 0 && y > 0) return M_PI / 2;
+    if (x == 0 && y < 0) return -M_PI / 2;
+    return 0;
 }
 
 struct ListNodeBase {
